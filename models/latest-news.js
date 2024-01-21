2a0376f626ce829490261a870607c7800424f911
hariesh-s <shariesh2002@gmail.com>
2024-01-21 23:03:16 +0400

    modelling & crud - latest news

---
+++ models/latest-news.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const latestNewsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+latestNewsSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("LatestNews", latestNewsSchema);
